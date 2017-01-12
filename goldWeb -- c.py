3f97250f813a7cb37e6721528ed51db9b8a8d046
Ryan Harper <11harryt@gmail.com>
2017-01-12 15:20:02 +0000

    Add files via upload

---
+++ goldWeb -- c.py
@@ -0,0 +1,268 @@
+"""
+
+- Prevent SQL injection (done?)
+
+install pip (thru cmd and get-pip.py) - then
+
+setx PATH "%PATH%;C:\Python34\Scripts"
+
+pip install beaker
+
+"""
+
+
+
+# Bottle Imports
+from bottle import app, route, run, static_file, template, request, redirect, app
+# Gets Store class from main Python file.
+from xmasTaskGold import Store
+# SQLite3 imports
+import sqlite3 as lite, sys, ast
+
+# Beaker Sessions
+from beaker.middleware import SessionMiddleware
+
+# Creates instance of the store class with the database and table names.
+#store = Store(lite.connect('festiveshop.db'))
+store = Store(lite.connect('festiveshop.db'))
+
+# Options for session
+sessions_opts = {
+    'session.type': 'memory',
+    'session.cookie_expires': 300,
+    'session.auto': True
+}
+
+# Creates the session
+bApp = SessionMiddleware(app(), sessions_opts)
+
+
+# Main Index Page
+@route('/')
+def index():
+    return template("index")
+
+# for CSS
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='static/')
+
+# Shopper Page - Login, Create Account, Back to Index
+@route('/shopper')
+def mainShopper():
+    return template('shopper')
+
+# Create Account Page
+# when post -> redirect to login page OR go back to newaccount if username taken
+@route('/newaccount', method='GET')
+def newAccount():
+    try:
+        pageError = request.query.error
+    except:
+        pageError = "Hello, Shopper!"
+    return template('newaccount', error=pageError)
+
+@route('/newaccount', method='POST')
+def createAccount():
+    store.c.execute("SELECT username FROM customers")
+    validUsernames = [u[0] for u in store.c.fetchall()]
+
+    firstname = request.forms.get('firstname')
+    lastname = request.forms.get('lastname')
+    address = request.forms.get('address')
+    username = request.forms.get('username')
+
+    # username correct -- create acc & redirect to login page
+    if username not in validUsernames: 
+        store.insert_data_cust(firstname, lastname, address, username)
+        return redirect('login')
+    # username incorrect -- go to taken page
+    else: 
+        return redirect('newaccount?error=Username Taken')
+
+
+# Login Page - if username wrong, error shows
+@route('/login', method='GET')
+def loginPage():
+    try:
+        pageError = request.query.error
+    except:
+        pageError = "Hello, Shopper!"
+
+    return template('login', error=pageError)
+
+
+@route('/login', method='POST')
+def loginPagePost():
+    store.c.execute("SELECT username FROM customers")
+    validUsernames = [u[0] for u in store.c.fetchall()]
+
+    # session
+    s = request.environ.get('beaker.session')
+
+    username = request.forms.get('username')
+    # U
+    if username in validUsernames:
+        store.c.execute("SELECT id FROM customers WHERE username='?'", (username,))
+        shopperid = store.c.fetchone()[0] # (was sid=..)
+        # Creates table for Basket [current orders]
+        store.createTableCurrentOrders()
+        # Sets session variable, sid, equal to the users id
+        s['sid'] = shopperid
+        return redirect('/shoppermain')
+    else:
+        return redirect('login?error=Wrong Username')
+
+### SHOPPER PAGE ###
+@route('/shoppermain', method='GET')
+def shopperPage():
+    # Gets session variables
+    s = request.environ.get('beaker.session')
+    # Gets sid from session
+    userId = s['sid']
+
+    store.c.execute("SELECT firstname FROM customers WHERE id=?", (userId,))
+    name = store.c.fetchone()[0]
+
+    return template('shoppermain', items=store.printItems(1), shopperName=name, basket=store.getItemDetails_CurrentOrder())
+
+# POST -> 'add to cart' and 'X' buttons, adds and removes to/from cart
+@route('/shoppermain', method='POST')
+def shopperPage2():
+
+    s = request.environ.get('beaker.session')
+    userId = s['sid']
+
+    # Multiple forms on page
+    for k in request.forms:
+        # ADDING ITEM TO BASKET
+        if k.startswith('numItems.'):
+            itemId = k.partition('.')[-1]
+            numItems = int(request.forms.get(k))
+            store.insert_data_current_order(itemId, numItems)
+        # REMOVING ITEM FROM BASKET
+        elif k =='orderItemId':
+            ordItemId = request.forms.get(k)
+            store.removeOrder(ordItemId)
+        # BUY BASKET
+        elif k == 'buyBasket':
+            basket = ast.literal_eval(request.forms.get(k)) # itemid = basket[4]
+            # buys basket, updating stock + inserting orders for basket 
+            for order in basket:
+                store.updateStock(order[4], -order[2])
+                store.insert_data_order(userId,order[4],order[2])
+                # Wipe table
+                store.createTableCurrentOrders()
+                # +1 in orders:
+                store.conn.execute("UPDATE customers SET orders = orders + ? WHERE id=?", (order[2], userId,))
+                store.conn.execute("UPDATE items SET quantity_bought = quantity_bought + ? WHERE id=?", (order[2], order[4],))
+                store.conn.commit()
+                print(order, file=sys.stderr)
+
+    return redirect('/shoppermain')
+
+# EDIT ACCOUNT 
+@route('/editaccount', method='GET')
+def editAccountPage():
+    # If there is an error (from URL) -- show it on page
+    try:
+        pageError = request.query.error
+    except:
+        pageError = "Hello, Shopper!"
+
+    # creates session & gets session variable [the user ids]
+    s = request.environ.get('beaker.session')
+    userId = s['sid']
+    
+    store.c.execute("SELECT * FROM customers WHERE ID=?", (userId,))
+    userDetails = store.c.fetchall()
+
+    return template('editaccount', details=userDetails, error=pageError)
+
+# EDIT ACCOUNT - POST, checks if username exists, if it does, reload the page with the error.
+@route('/editaccount', method='POST')
+def editAccountPagePost():
+    store.c.execute("SELECT username FROM customers")
+    validUsernames = [u[0] for u in store.c.fetchall()]
+
+    s = request.environ.get('beaker.session')
+    custId = s['sid']
+
+    colToEdit = request.forms.get('toEdit')
+    newValue = request.forms.get('newValue')
+
+    if colToEdit =='username' and newValue in validUsernames: # bad --> username taken
+        return redirect('/editaccount?error=Username Taken')
+    else:
+        store.updateUserAcc(custId, colToEdit, newValue)
+        return redirect('/editaccount')
+
+
+# ORDER HISTORY
+@route('/orders')
+def orderHistory():
+    s = request.environ.get('beaker.session')
+    userId = s['sid']
+    orderDetails = store.showOrderHistory(userId)
+    return template('orders', details = orderDetails)
+
+# DELETE ACCOUNT
+@route('/delete', method='GET')
+def deleteAccount():
+    s = request.environ.get('beaker.session')
+    userId = s['sid']
+    return template('delete')
+
+@route('/delete', method='POST')
+def delete():
+    s = request.environ.get('beaker.session')
+    userId = s['sid']
+    store.deleteUser(userId)
+    redirect('/shopper')
+
+### WORKER PAGES ###
+@route('/worker', method='GET')
+def workerPage1():
+    return template('worker', items=store.printItems(2))
+
+# Gets ITEMID of the item whose stock is being changed.
+@route('/worker', method='POST')
+def workerPage2():
+    for k in request.forms:
+        if k.startswith('newStock.'):
+            itemId = k.partition('.')[-1]
+            numStock = request.forms.get(k)
+            store.updateStock(itemId,numStock)
+
+    return redirect('worker')
+
+### ADD NEW ITEM PAGE ###
+@route('/newitem', method='GET')
+def newItemPage1():
+    return template('newitem')
+
+# DOES STUFF WITH ADD ITEM FORM --> adds item
+@route('/newitem', method='POST')
+def newItemPage2():
+    nameItem = request.forms.get('nameItem')
+    costItem = request.forms.get('costItem')
+    stockItem = request.forms.get('stockItem')
+    catItem = request.forms.get('catItem')
+    store.addItemWeb(nameItem, costItem, stockItem, catItem)
+    return template('newitem')
+
+
+### TOP 5 ### 
+
+# TOP 5 ITEMS
+@route('/t5items')
+def top5Items():
+    top5i = store.top5products()
+    return template('top5items', top5items = top5i)
+
+@route('/t5customers')
+def top5Customers():
+    return template('top5customers')
+
+run(app=bApp, host='localhost', port=8080)
+
---
+++ xmasTaskGold -- c.py
@@ -0,0 +1,279 @@
+"""
+Ryan Harper
+
+Christmas Task : GOLD
+
+Extra Python Packages Used:
+    - Bottle [web server]
+
+
+------------------------------------------------------------------
+'Did you hear about the movie they made about the database admin?'
+
+'There was NoSQL.'
+------------------------------------------------------------------
+
+
+
+"""
+
+import sqlite3 as lite, os, sys
+
+# BASE ITEMS, WILL BE ENTERED ON FIRST RUN
+items = [{'name': 'Baubles 50 pk', 'purchCost': 3.00, 'saleCost': 5.00, 'stock_lvl': 30,'cat':'xmas_decs', 'qtyBought':3},
+            {'name': 'Tinsel Red', 'purchCost':0.50, 'saleCost': 1.00, 'stock_lvl': 25,'cat':'xmas_decs', 'qtyBought':1},
+            {'name': 'Tinsel Gold', 'purchCost': 0.50, 'saleCost': 1.00, 'stock_lvl': 35,'cat':'xmas_decs', 'qtyBought':2},
+            {'name': 'Christmas Tree', 'purchCost': 59.99, 'saleCost': 80.00, 'stock_lvl': 20,'cat':'xmas_decs', 'qtyBought':0},
+            {'name': 'Tree Lights', 'purchCost': 3.00, 'saleCost': 7.00, 'stock_lvl': 30,'cat':'xmas_decs', 'qtyBought':0},
+            {'name': 'Tree Star', 'purchCost': 1.00, 'saleCost': 2.00, 'stock_lvl': 25,'cat':'xmas_decs', 'qtyBought':0},
+            {'name': 'Turkey', 'purchCost': 9.99, 'saleCost': 20.00, 'stock_lvl': 20,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Potatoes 2kg', 'purchCost': 0.80, 'saleCost': 1.50, 'stock_lvl': 35,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Carrots 1kg', 'purchCost': 0.60, 'saleCost': 1.00, 'stock_lvl': 25,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Parsnips 1kg', 'purchCost': 0.80, 'saleCost': 1.80, 'stock_lvl': 15,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Pigs in Blankets 20pk', 'purchCost': 1.20, 'saleCost': 2.00, 'stock_lvl': 40,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Stuffing 200g', 'purchCost': 0.90, 'saleCost': 1.50, 'stock_lvl': 29,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Celebrations Box', 'purchCost': 1.50, 'saleCost': 3.00, 'stock_lvl': 18,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Prosecco 75cl', 'purchCost': 4.99, 'saleCost': 6.50, 'stock_lvl': 13,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'Mixed Nuts 500g', 'purchCost': 1.20, 'saleCost': 3.00, 'stock_lvl': 27,'cat':'xmas_food', 'qtyBought':0},
+            {'name': 'iPad Air 2', 'purchCost': 399.99, 'saleCost': 499.99, 'stock_lvl': 18,'cat':'xmas_electricals', 'qtyBought':0},
+            {'name': 'Fitbit', 'purchCost':49.99, 'saleCost':75.00, 'stock_lvl': 23,'cat':'xmas_electricals', 'qtyBought':0},
+            {'name': 'Xbox One S', 'purchCost': 199.99, 'saleCost': 250.00, 'stock_lvl': 32,'cat':'xmas_electricals', 'qtyBought':0},
+            {'name': 'Kindle Paperwhite', 'purchCost': 74.99, 'saleCost': 104.49, 'stock_lvl': 22,'cat':'xmas_electricals', 'qtyBought':0},
+            {'name': 'Parrot Drone', 'purchCost': 410.00, 'saleCost': 499.99, 'stock_lvl': 11,'cat':'xmas_electricals', 'qtyBought':0},
+            {'name': 'Smart Kettle', 'purchCost': 59.99, 'saleCost': 74.50, 'stock_lvl': 24,'cat':'xmas_electricals', 'qtyBought':0},
+            {'name': 'Oral-B Electric Toothbrush', 'purchCost':69.99, 'saleCost': 94.99, 'stock_lvl': 13,'cat':'xmas_electricals', 'qtyBought':0}]
+
+# BASE CUSTOMER LIST
+customers = [{'firstname':'Ryan', 'lastname':'Harper', 'address':'123 Example Road', 'username':'rtharper', 'orders':2},
+            {'firstname':'John', 'lastname':'Smith', 'address':'456 Road Avenue', 'username':'jsmith', 'orders':1},
+            {'firstname':'Lucy', 'lastname':'Jones', 'address':'321 Mini Street', 'username':'ljones', 'orders':0},
+            {'firstname':'Luke', 'lastname':'Hayward', 'address':'45 Withers Close', 'username':'lukehay', 'orders':0},
+            {'firstname':'Jade', 'lastname':'Robson', 'address':'63 Sutton Lane', 'username':'jaderobson', 'orders':0},
+            {'firstname':'Zara', 'lastname':'Evans', 'address':'55 Wheel Road', 'username':'zevans', 'orders':0},
+            {'firstname':'Jack', 'lastname':'Mason', 'address':'46 Dover Road', 'username':'jamason', 'orders':0}]
+
+# BASE ORDER LIST
+orders = [
+            {'custId':1, 'itemId':2,  'quantity':1, 'groupId':1},
+            {'custId':1, 'itemId':3, 'quantity':2, 'groupId':1},
+            {'custId':2, 'itemId':1, 'quantity':3, 'groupId':2}
+        ]
+
+workerPass = "pass123word"
+
+# Store Class
+class Store:
+    # initialise object
+    def __init__(self, conn):
+        self.conn = conn # db connection
+        self.c = self.conn.cursor() # create cursor to get output
+
+    # if first run of program & no db; creates table & inserts data if it doesn't exist
+    def initDb(self):
+        self.createTableItems()
+        self.createTableCustomers()
+        self.createTableOrders()
+
+        for i in items:
+            self.insert_data_stock(i['name'], i['purchCost'], i['saleCost'], i['stock_lvl'], i['cat'], i['qtyBought'])
+
+        for i in customers:
+            self.insert_data_cust(i['firstname'], i['lastname'], i['address'], i['username'], i['orders'])
+
+        for i in orders:
+            self.insert_data_order(i['custId'], i['itemId'], i['quantity'])
+
+        self.conn.commit()
+
+    # --------------------- CREATE TABLES ---------------------
+
+    # for first run -- ITEMS
+    def createTableItems(self):
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT NOT NULL,
+        purchCost REAL NOT NULL, saleCost REAL NOT NULL, stock_lvl INTEGER NOT NULL, cat TEXT NOT NULL, quantity_bought INTEGER NOT NULL)""")
+
+    # for first run -- CUSTOMERS
+    def createTableCustomers(self):
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS customers (id INTEGER PRIMARY KEY, firstname TEXT NOT NULL,
+        lastname TEXT NOT NULL, address TEXT NOT NULL, username TEXT NOT NULL, orders INTEGER NOT NULL, top5 BOOLEAN)""")
+
+    # for first run -- ORDERS
+    def createTableOrders(self):
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, custId INTEGER,
+        itemId INTEGER, quantity INTEGER NOT NULL, cost REAL NOT NULL, profit REAL NOT NULL, FOREIGN KEY(itemId) REFERENCES items(id), FOREIGN KEY(custId) REFERENCES customers(id))""")
+
+    # --------------------- TABLE FOR CURRENT ORDER ---------------------
+
+    def createTableCurrentOrders(self):
+        # Deletes if table already exists (useful for when logging into another acc. or when basket is bought)
+        self.conn.execute("DROP TABLE IF EXISTS current_order")
+        self.conn.execute("""CREATE TABLE current_order (id INTEGER PRIMARY KEY, itemId INTEGER, quantity INTEGER, cost REAL, FOREIGN KEY(itemId) REFERENCES items(id))""")
+
+    def insert_data_current_order(self, itemId, qty):
+        self.c.execute("SELECT saleCost FROM items WHERE id=?", (itemId,))
+        cost = self.c.fetchall()[0][0]
+        cost = qty * float(cost)
+        self.conn.execute("INSERT INTO current_order (itemId, quantity, cost) VALUES (?, ?, ?)", (itemId, qty, cost))
+        self.conn.commit()
+
+    def getItemDetails_CurrentOrder(self):
+        self.c.execute("SELECT id, itemId, quantity, round(cost, 2) FROM current_order") #[0]=id, [1]=itemId, [2]=qty, [3]=cost
+        currentOrdersTuples = self.c.fetchall()
+        # creates list of lists instead of list of tuples --> becomes mutable
+        currentOrders = [list(e) for e in currentOrdersTuples]
+
+        # replaces itemId with item name
+        for n in currentOrders:
+            self.c.execute("SELECT name FROM items WHERE id=?", (n[1],))
+            n.append(n[1]) # used for when buying -- adds item ID to list
+            n[1] = self.c.fetchall()[0][0] # sets ID equal to name
+
+        return currentOrders
+
+    def removeOrder(self, id_):
+        self.conn.execute("DELETE FROM current_order WHERE id=?", (id_,))
+        self.conn.commit()
+
+    # --------------------- INSERTS ---------------------
+
+    # ADD NEW -- ITEM
+    def insert_data_stock(self, name, purchCost, saleCost, stock_lvl, cat, qtyBought=0):
+        self.conn.execute("INSERT INTO items (name, purchCost, saleCost, stock_lvl, cat, quantity_bought) VALUES (?, ?, ?, ?, ?, ?)", (name, purchCost, saleCost, stock_lvl, cat, qtyBought))
+        self.conn.commit()
+
+
+    # ADD NEW -- CUSTOMER
+    def insert_data_cust(self,fname,lname,addr,usrname,orders=0):
+        self.conn.execute("INSERT INTO customers (firstname, lastname, address, username, orders) VALUES (?, ?, ?, ?, ?)", (fname, lname, addr, usrname, orders))
+        self.conn.commit()
+        self.top5Customers()
+
+    # ADD NEW -- ORDER
+    def insert_data_order(self, custId, itemId, qty):
+        # Gets bought item sale cost [what the customer bought the item for]
+        self.c.execute("SELECT saleCost FROM items WHERE id=?", (itemId,))
+        costTotal = self.c.fetchone()[0]
+        # Gets bought item purchase cost [what the store bought the item for]
+        self.c.execute("SELECT purchCost FROM items WHERE id=?", (itemId,))
+        purchCost = self.c.fetchone()[0]
+        # Calculates total cost
+        cost = int(qty) * costTotal
+        # Calculates how much profit earned from order
+        profit = cost-(int(qty)*purchCost)
+        # Inserts order details into orders table & updates customers orders
+        self.conn.execute("INSERT INTO orders (custId, itemId, quantity, cost, profit) VALUES (?, ?, ?, ?, ?)", (custId, itemId, qty, cost, profit))
+        self.conn.commit()
+        self.conn.execute("UPDATE customers SET orders = orders + ? WHERE id=?", (qty,custId,))
+        self.conn.commit()
+        # Runs top5customers function so if user is now top 5 it updates the customers table and top5 list.
+        self.top5Customers()
+
+    # --------------------- others ---------------------
+
+    def deleteUser(self, custId):
+        self.conn.execute("DELETE FROM customers WHERE id=?", (custId,))
+        self.conn.execute("DELETE FROM orders WHERE custId=?", (custId,))
+        self.conn.commit()
+        # Updates top5customers 
+        self.top5Customers()
+
+    def addItemWeb(self, name, purchCost, saleCost, stock_lvl, cat):
+        self.insert_data_stock(name, purchCost, saleCost, stock_lvl, cat)
+        self.conn.commit()
+
+
+    def updateStock(self, idn, change):
+        self.c.execute("SELECT stock_lvl FROM items WHERE id=?", (idn,)) # gets stock_lvl of the item corresponding to id
+        currentStock = self.c.fetchall()[0][0] # gets stock_lvl
+        newStock = currentStock + int(change) # alters stock level
+        self.conn.execute("UPDATE items SET stock_lvl = ? WHERE id=?", (newStock, idn,)) # updates stock level to new stock level
+        self.conn.commit() # commits changes to table
+
+    # SHOW ORDER HISTORY FOR USER
+
+    def showOrderHistory(self,custId):
+        self.c.execute("SELECT * FROM orders WHERE custId=?", (custId,))
+        ordersTuples = self.c.fetchall()
+
+        self.c.execute("SELECT name FROM items WHERE id=1")
+        name = self.c.fetchall()[0][0]
+
+        orders = [list(el) for el in ordersTuples]
+
+        for i in orders:
+            self.c.execute("SELECT name FROM items WHERE id=?", (i[2],))
+            i[2] = self.c.fetchall()[0][0]
+
+        return orders
+
+
+    def printItems(self, usr):
+        # For shoppers (doesn't show item where stock level = 0 )
+        if usr == 1: 
+            self.c.execute("SELECT id, name, round(purchCost, 2), round(saleCost, 2), stock_lvl, cat, quantity_bought FROM items WHERE stock_lvl > 0")
+            return self.c.fetchall()
+        # For workers
+        else:
+            self.c.execute("SELECT id, name, round(purchCost, 2), round(saleCost, 2), stock_lvl, cat, quantity_bought FROM items") 
+            return self.c.fetchall()
+
+    def updateUserAcc(self, custId, col, new):
+        self.conn.execute("UPDATE customers SET ? = '?' WHERE id=?", (col, new, custId,))
+        self.conn.commit()
+
+    # ~~ TOP 5 ~~ 
+
+    # TOP 5 PRODUCTS
+
+    def top5products(self):
+        self.c.execute("SELECT name, quantity_bought, stock_lvl FROM items")
+        itemList = self.c.fetchall()
+        # Sorts item list by quantity bought, then splices so it is only top 5
+        itemListT5 = sorted(itemList, key=lambda t: t[1], reverse=True)[:5]
+
+        return itemListT5
+
+    def top5custs(self):
+        self.c.execute("SELECT id, firstname, lastname FROM customers")
+        custIdList = self.c.fetchall() # [(id, fn, ln)] -- gets customer info and id
+
+        profitList = []
+        # collects profit made by customer and amount spent by customer
+        for i in custIdList:
+            self.c.execute("SELECT profit FROM orders WHERE custId=?", (i[0],))
+            profits = self.c.fetchall()
+
+            self.c.execute("SELECT cost FROM orders WHERE custId=?", (i[0],))
+            costs = self.c.fetchall()
+
+            #
+            profitList.append((i[0], float("%.2f"%sum([p[0] for p in profits])), i[1], i[2], float("%.2f"%sum([c[0] for c in costs]))))
+
+        # [(UserId, Profit, Fname, Lname, Costs)]
+        #print(profitList)
+
+        # Sorts customerList by profit made, then splices so it is only top 5
+        # reverse = True sorts it from z-a
+        # key is what the list is sorted by
+        custListT5 = sorted(profitList, key=lambda t: t[1], reverse=True)[:5] # sorts list of tuples by PROFIT, then splices so its only top 5
+
+        for i in custListT5:
+            self.conn.execute("UPDATE customers SET top5 = 1 WHERE id=?", (i[0],))
+            self.conn.commit()
+
+        return custListT5
+
+"""
+def main():
+    dbFilename = 'festiveshop.db'
+    dbNew = not os.path.exists(dbFilename)
+    store = Store(lite.connect(dbFilename))
+
+    if dbNew:
+        store.initDb()
+    else:
+        print("Database exists, assuming table and initial items do too.")
+
+
+main()
+"""
